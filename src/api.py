de34664eb06f61e0234d020c2ec0a918ae58e3e9
Arina Okshus <me@chameleon-lizard.ru>
2023-10-31 03:34:57 +0300

    feature: api - added basic API to put songs in the queue and get info about queue and playback

---
+++ src/api.py
@@ -0,0 +1,60 @@
+import logging
+
+import fastapi
+import pydantic
+
+from src import server
+from src import utils
+
+app = fastapi.FastAPI()
+
+player = server.Player()
+player.start()
+
+
+class AddSongBaseModel(pydantic.BaseModel):
+    url: str
+
+
+@app.post("/add_song/{url}")
+def play_song(
+    add_song: AddSongBaseModel,
+):
+    if not utils.check_url(url=add_song.url):
+        logging.error(msg=f'Incorrect URL: {add_song.url}')
+
+        return {
+            'Result': f'Incorrect URL: {add_song.url}',
+        }
+
+    try:
+        song = utils.download_song(add_song.url)
+    except ValueError as e:
+        logging.error(msg=f'Youtube-dl returned error: {str(e)}, URL: {add_song.url}')
+        return {
+            'Result': f'Youtube-dl returned error: {str(e)}, URL: {add_song.url}',
+        }
+
+    try:
+        player.add_song(song)
+    except server.playsound.PlaysoundException as e:
+        logging.error(msg=f'Playsound error: {str(e)}, Song: {song}')
+        return {
+            'Result': f'Playsound error: {str(e)}, Song: {song}',
+        }
+
+    return {
+        'Result': song.to_dict()
+    }
+
+
+@app.get("/now_playing")
+def now_playing():
+    return player.now_playing.to_dict()
+
+
+@app.get("/history")
+def get_history():
+    return {
+        'Result': list(map(lambda _: _.to_dict(), player.history))
+    }
